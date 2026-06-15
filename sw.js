b467e8523d84790533905b2fa7d51e83a1172785
nageshpn999-coder <nageshpn999@gmail.com>
2026-06-15 09:21:33 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+// OnlineLand Service Worker
+const CACHE_NAME = 'onlineland-v1';
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
